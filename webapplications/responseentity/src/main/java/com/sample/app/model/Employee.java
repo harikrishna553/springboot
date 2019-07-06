6d9cfaa01bbd125a55dc5c0a9522701a619d09dc
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-07-06 14:03:35 +0530

    add ResponseEntity example

---
+++ webapplications/responseentity/src/main/java/com/sample/app/model/Employee.java
@@ -0,0 +1,32 @@
+package com.sample.app.model;
+
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
