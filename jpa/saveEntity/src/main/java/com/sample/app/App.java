5018c5fe374191819212dc866a4228c222680599
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-07-08 12:22:32 +0530

    persist an entity

---
+++ jpa/saveEntity/src/main/java/com/sample/app/App.java
@@ -0,0 +1,37 @@
+package com.sample.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sample.app.model.Employee;
+import com.sample.app.repository.EmployeeRepository;
+
+@SpringBootApplication
+public class App {
+	
+	private static void printEmployee(Employee emp) {
+		System.out.println("------------------------------");
+		System.out.println("Id : " + emp.getId());
+		System.out.println("firstName : " + emp.getFirstName());
+		System.out.println("lastName : " + emp.getLastName());
+		System.out.println("------------------------------");
+	}
+	
+	public static void main(String args[]) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(EmployeeRepository employeeRepository) {
+		return (args) -> {
+			Employee emp1 = Employee.builder().firstName("Ram").lastName("Gurram").build();
+
+			Employee persistedEmployee = employeeRepository.save(emp1);
+			
+			printEmployee(persistedEmployee);
+		};
+	}
+
+}
