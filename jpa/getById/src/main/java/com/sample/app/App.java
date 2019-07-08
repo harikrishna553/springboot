45a8a65793d96ef7dafb0344b00d3f776e46d49b
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-07-08 15:05:33 +0530

    get entity by id

---
+++ jpa/getById/src/main/java/com/sample/app/App.java
@@ -0,0 +1,46 @@
+package com.sample.app;
+
+import java.util.Optional;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sample.app.model.Employee;
+import com.sample.app.repository.EmployeeRepository;
+
+@SpringBootApplication
+public class App {
+
+	private static void printEmployee(Employee emp) {
+		System.out.println("------------------------------");
+		System.out.println("Id : " + emp.getId());
+		System.out.println("firstName : " + emp.getFirstName());
+		System.out.println("lastName : " + emp.getLastName());
+		System.out.println("------------------------------");
+	}
+
+	public static void main(String args[]) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(EmployeeRepository employeeRepository) {
+		return (args) -> {
+			Employee emp1 = Employee.builder().firstName("Ram").lastName("Gurram").build();
+
+			Employee persistedEmployee = employeeRepository.save(emp1);
+
+			Optional<Employee> empOpt = employeeRepository.findById(persistedEmployee.getId());
+
+			if (!empOpt.isPresent()) {
+				System.out.println("Employee is not exist for given id");
+			} else {
+				printEmployee(empOpt.get());
+			}
+
+		};
+	}
+
+}
