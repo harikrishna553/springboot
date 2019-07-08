71324db899b135fdfe940cc8305a72727a6edfd3
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-07-08 20:14:04 +0530

    Delete an entity

---
+++ jpa/deleteEntity/src/main/java/com/sample/app/App.java
@@ -0,0 +1,48 @@
+package com.sample.app;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sample.app.model.Employee;
+import com.sample.app.repository.EmployeeRepository;
+
+@SpringBootApplication
+public class App {
+
+	private static void printEmployee(Employee emp) {
+		System.out.println("------------------------------");
+		System.out.println("Id : " + emp.getId());
+		System.out.println("firstName : " + emp.getFirstName());
+		System.out.println("lastName : " + emp.getLastName());
+		System.out.println("------------------------------");
+	}
+
+	public static void main(String args[]) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(EmployeeRepository employeeRepository) {
+		return (args) -> {
+			Employee emp1 = Employee.builder().firstName("Ram").lastName("Gurram").build();
+
+			Employee persistedEmployee1 = employeeRepository.save(emp1);
+
+			long totalEmployees = employeeRepository.count();
+
+			System.out.println("Total Employees : " + totalEmployees);
+			System.out.println("Deleting an employee");
+
+			employeeRepository.delete(persistedEmployee1);
+			totalEmployees = employeeRepository.count();
+
+			System.out.println("Total Employees : " + totalEmployees);
+		};
+	}
+
+}
