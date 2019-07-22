2dc7a93742c28da35fcec29069a0bc9937fbbc40
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-07-22 20:57:28 +0530

    Update the record by id

---
+++ jdbc/updateRecordById/src/main/java/com/sample/app/repository/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.sample.app.repository;
+
+import java.util.List;
+
+import com.sample.app.entity.Employee;
+
+public interface EmployeeRepository {
+
+	public Employee save(Employee emp);
+	
+	public List<Employee> all();
+	
+	public Employee get(int id);
+	
+	public Employee update(Employee emp);
+}
