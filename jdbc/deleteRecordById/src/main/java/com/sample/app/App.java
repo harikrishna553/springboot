17e0429e21af13d2f2288a562594155898a86173
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-07-23 08:20:03 +0530

    Delete a record by id

---
+++ jdbc/deleteRecordById/src/main/java/com/sample/app/App.java
@@ -0,0 +1,55 @@
+package com.sample.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.sample.app.entity.Employee;
+import com.sample.app.repository.EmployeeRepository;
+
+@SpringBootApplication
+public class App {
+	private static final Logger log = LoggerFactory.getLogger(App.class);
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	public static void main(String args[]) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo() {
+		return (args) -> {
+			log.info("Creating tables");
+
+			jdbcTemplate.execute("DROP TABLE employees IF EXISTS");
+			jdbcTemplate.execute(
+					"CREATE TABLE employees (id serial, first_name VARCHAR(255), last_name VARCHAR(255), PRIMARY KEY(id)) ");
+
+			Employee emp1 = new Employee("Ram", "Gurram");
+			Employee emp2 = new Employee("Moutwika", "Maj");
+			Employee emp3 = new Employee("Anusha", "R");
+
+			emp1 = employeeRepository.save(emp1);
+			employeeRepository.save(emp2);
+			employeeRepository.save(emp3);
+
+			employeeRepository.printAll();
+
+			System.out.println("Deleting an employee with id " + emp1.getId());
+			employeeRepository.delete(emp1.getId());
+
+			employeeRepository.printAll();
+		};
+	}
+
+}
---
+++ jdbc/deleteRecordById/src/main/java/com/sample/app/entity/Employee.java
@@ -0,0 +1,53 @@
+package com.sample.app.entity;
+
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+
+	public Employee(int id, String firstName, String lastName) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Employee(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Employee() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Employee [id=").append(id).append(", firstName=").append(firstName).append(", lastName=")
+				.append(lastName).append("]");
+		return builder.toString();
+	}
+
+}
