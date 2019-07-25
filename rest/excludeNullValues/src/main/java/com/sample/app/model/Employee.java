5b5a016ce1e9e20f0bf16b2d1969386efdc416ba
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-07-25 13:43:09 +0530

    Exclude null values from the response

---
+++ rest/excludeNullValues/src/main/java/com/sample/app/model/Employee.java
@@ -0,0 +1,61 @@
+package com.sample.app.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties({"employee_id"})
+@JsonPropertyOrder({"last_name", "first_name"})
+public class Employee {
+
+	@JsonProperty("employee_id")
+	private int id;
+
+	@JsonProperty("first_name")
+	private String firstName;
+
+	@JsonProperty("last_name")
+	private String lastName;
+
+	public Employee(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Employee [id=").append(id).append(", firstName=").append(firstName).append(", lastName=")
+				.append(lastName).append("]");
+		return builder.toString();
+	}
+
+}
