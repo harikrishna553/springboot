f9a299997024779e90c0498db7b6e79cd73b410f
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-07-26 09:05:38 +0530

    Restrict response data to xml only

---
+++ rest/restrictResponseDataToSpecificType/src/main/java/com/sample/app/repository/EmployeeRepository.java
@@ -0,0 +1,42 @@
+package com.sample.app.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.sample.app.model.Employee;
+
+@Repository
+public class EmployeeRepository {
+
+	private static final List<Employee> emps = new ArrayList<> ();
+	
+	private static int counter = 4;
+	
+	static {
+		Employee emp1 = new Employee(1, "Sunil", "Dayanand");
+		Employee emp2 = new Employee(2, null, "Shetty");
+		Employee emp3 = new Employee(3, "Ram", "Anand");
+		Employee emp4 = new Employee(4, "Akansha", null);
+		
+		emps.add(emp1);
+		emps.add(emp2);
+		emps.add(emp3);
+		emps.add(emp4);
+		
+	}
+	
+	public List<Employee> all(){
+		return emps;
+	}
+	
+	public Employee add(Employee emp) {
+		emp.setId(counter);
+		counter++;
+		
+		emps.add(emp);
+		return emp;
+	}
+	
+}
