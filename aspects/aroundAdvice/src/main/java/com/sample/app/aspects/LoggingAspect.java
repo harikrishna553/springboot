5c6a2ecb2ad944468b2837d37b82c6e615a6904e
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-07-30 14:42:37 +0530

    @Around advice demo

---
+++ aspects/aroundAdvice/src/main/java/com/sample/app/aspects/LoggingAspect.java
@@ -0,0 +1,32 @@
+package com.sample.app.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Aspect
+public class LoggingAspect {
+
+	@Around("@annotation(com.sample.app.annotations.Loggable)")
+	public Object executionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+		String methodName = joinPoint.getSignature().getName();
+
+		System.out.println("Execution of the method " + methodName + " started.");
+
+		long startTime = System.currentTimeMillis();
+
+		// Proceed with the next advice or target method invocation
+		Object result = joinPoint.proceed();
+
+		long endTime = System.currentTimeMillis();
+
+		System.out.println("Execution of the method " + methodName + " finished.");
+		System.out.println("Execution of the method takes " + (endTime - startTime) + " milliseconds.");
+
+		return result;
+	}
+
+}
+
