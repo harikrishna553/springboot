5c9d8c0d46d5a8f0c310059c66958a07d93b4315
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-07-30 14:33:58 +0530

    @After Advice Demo

---
+++ aspects/afterAdvice/src/main/java/com/sample/app/aspects/LoggingAspect.java
@@ -0,0 +1,27 @@
+package com.sample.app.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Aspect
+public class LoggingAspect {
+
+	@Before("@annotation(com.sample.app.annotations.Loggable)")
+	public void logMeBefore(JoinPoint joinPoint) {
+		String methodName = joinPoint.getSignature().getName();
+
+		System.out.println("Execution of the method " + methodName + " started.");
+	}
+
+	@After("@annotation(com.sample.app.annotations.Loggable)")
+	public void logMeAfter(JoinPoint joinPoint) {
+		String methodName = joinPoint.getSignature().getName();
+
+		System.out.println("Execution of the method " + methodName + " finished.");
+	}
+
+}
