15d55494d4663d1164ca7c1c464654fdc613a1bb
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-08-13 09:25:32 +0530

    Insert new document if not exist

---
+++ mongoDB/insertNewDocumentIfNotExist/src/main/java/com/sample/app/App.java
@@ -0,0 +1,74 @@
+package com.sample.app;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.mongodb.MongoClient;
+import com.sample.app.entity.Employee;
+import com.sample.app.repository.EmployeeRepository;
+
+@SpringBootApplication
+public class App {
+	@Value("${spring.data.mongodb.database}")
+	private String database;
+
+	@Value("${spring.data.mongodb.host}")
+	private String host;
+
+	@Value("${spring.data.mongodb.port}")
+	private int port;
+
+	@Autowired
+	private EmployeeRepository empRepository;
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(App.class, args);
+	}
+
+	public void dropPreviousData() {
+		MongoClient mongoClient = new MongoClient(host, port);
+
+		MongoOperations mongoOps = new MongoTemplate(mongoClient, database);
+
+		mongoOps.dropCollection("employees");
+	}
+
+	@Bean
+	public CommandLineRunner demo() {
+		return (args) -> {
+
+			dropPreviousData();
+
+			Employee emp1 = new Employee("Phalgun", "Garimella");
+		
+			emp1 = empRepository.insert(emp1);
+			
+			emp1.setFirstName("ram");
+			emp1.setLastName("Gurram");
+			System.out.println("Trying to insert emp with same id again");
+			
+			try {
+				empRepository.insert(emp1);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			List<Employee> emps = empRepository.findAll();
+
+			System.out.println("\nAll Employees");
+			for (Employee emp : emps) {
+				System.out.println(emp);
+			}
+
+		};
+	}
+}
