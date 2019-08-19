226d1cf256137124199264d26aae2d3f29b4f790
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-08-19 10:10:59 +0530

    Add repository or dao layer

---
+++ microservice/daoLayer/src/main/java/com/sample/app/entity/Employee.java
@@ -0,0 +1,51 @@
+package com.sample.app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private String firstName;
+	private String lastName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Employee [id=").append(id).append(", firstName=").append(firstName).append(", lastName=")
+				.append(lastName).append("]");
+		return builder.toString();
+	}
+
+}
