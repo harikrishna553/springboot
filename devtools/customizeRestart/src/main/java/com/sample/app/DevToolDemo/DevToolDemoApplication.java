c63e85ddf89ed8df06539cd4bfb94980c38e955c
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-09-04 13:41:22 +0530

    Customize restart

---
+++ devtools/customizeRestart/src/main/java/com/sample/app/DevToolDemo/DevToolDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sample.app.DevToolDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevToolDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevToolDemoApplication.class, args);
+	}
+
+}
