f500a1b028cfea99eb4467b7e8528799cd4e958b
Hari Krishna Gurram <hari.krishna.gurram@sap.com>
2019-09-05 20:13:39 +0530

    @Timed annotation demo

---
+++ actuator/timedAnnotation/src/main/java/com/sample/app/controller/HelloController.java
@@ -0,0 +1,37 @@
+package com.sample.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
+@RestController
+public class HelloController {
+	
+	private final Counter helloCounter;
+	
+	public HelloController(MeterRegistry meterRegistry) {
+		helloCounter = Counter.builder("api.hello").register(meterRegistry);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Welcome to spring boot developement";
+	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		helloCounter.increment();
+		return "Good Morning";
+	}
+	
+	@RequestMapping("/welcome")
+	@Timed(value = "api.welcome")
+	public String welcome() {
+		helloCounter.increment();
+		return "Hello user, have a nice day";
+	}
+
+}
