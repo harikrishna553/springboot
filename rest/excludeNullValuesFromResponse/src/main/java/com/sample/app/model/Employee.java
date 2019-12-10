f8fe80aeaad5b8dd29c23a07e4de974dd38a69e0
Krishna <harikrishna553@gmail.com>
2019-12-10 15:24:09 +0530

    Exclude null values from response

---
+++ rest/excludeNullValuesFromResponse/src/main/java/com/sample/app/model/Employee.java
@@ -0,0 +1,50 @@
+package com.sample.app.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+
+	public Employee(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Employee [id=").append(id).append(", firstName=").append(firstName).append(", lastName=")
+				.append(lastName).append("]");
+		return builder.toString();
+	}
+
+}
