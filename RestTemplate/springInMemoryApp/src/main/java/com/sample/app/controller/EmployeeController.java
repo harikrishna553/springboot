c069ba4b21533ec7ea34e83ece3937740e75a269
Your Name <you@example.com>
2020-03-09 15:43:37 +0530

    Add swagger Documentation

--- RestTemplate/springInMemoryApp/src/main/java/com/sample/app/controller/EmployeeController.java
+++ RestTemplate/springInMemoryApp/src/main/java/com/sample/app/controller/EmployeeController.java
@@ -13,21 +13,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sample.app.model.Employee;
 import com.sample.app.util.EmployeeUtil;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("api/v1/")
+@Api(tags = { "This section contains all user Speicifc Operations" })
 public class EmployeeController {
 
+	@ApiOperation(value = "Retrieve all the employee details", notes = "This API Retrieve all the employee details")
 	@RequestMapping(value = "employees", method = RequestMethod.GET)
 	public ResponseEntity<Collection<Employee>> all() {
 		return ResponseEntity.ok(EmployeeUtil.all());
 	}
 
+	@ApiOperation(value = "Create new employee", notes = "Create new employee")
 	@RequestMapping(value = "employees", method = RequestMethod.POST)
 	public ResponseEntity<Employee> create(@RequestBody Employee emp) {
 		return new ResponseEntity<>(EmployeeUtil.create(emp), HttpStatus.CREATED);
 
 	}
 
+	@ApiOperation(value = "Get user", notes = "Get employee by id")
 	@RequestMapping(value = "employees/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Employee> byId(@PathVariable int id) {
 		Employee emp = EmployeeUtil.byId(id);
@@ -40,6 +47,7 @@ public class EmployeeController {
 
 	}
 
+	@ApiOperation(value = "Delete employee by id", notes = "Delete employee by id")
 	@RequestMapping(value = "employees/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Employee> deleteById(@PathVariable int id) {
 		Employee emp = EmployeeUtil.byId(id);
@@ -51,6 +59,7 @@ public class EmployeeController {
 		return ResponseEntity.ok(EmployeeUtil.delete(id));
 	}
 
+	@ApiOperation(value = "Update employee by id", notes = "Update employee by id")
 	@RequestMapping(value = "employees/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Employee> updateById(@PathVariable int id, @RequestBody Employee emp) {
 
