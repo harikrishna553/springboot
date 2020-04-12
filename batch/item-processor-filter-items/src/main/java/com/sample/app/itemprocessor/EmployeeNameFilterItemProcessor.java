6bbd6c9d16c2abd71eb544464682cd05350b8faf
Your Name <you@example.com>
2020-04-12 15:53:27 +0530

    ItemProcessor to filter items

---
+++ batch/item-processor-filter-items/src/main/java/com/sample/app/itemprocessor/EmployeeNameFilterItemProcessor.java
@@ -0,0 +1,26 @@
+package com.sample.app.itemprocessor;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import com.sample.app.entity.Employee;
+
+@Component
+public class EmployeeNameFilterItemProcessor implements ItemProcessor<Employee, Employee>{
+	
+	private static final List<String> NAMES_TO_EXCLUDE = Arrays.asList("Rama", "Sailu");
+
+	@Override
+	public Employee process(Employee emp) throws Exception {
+		
+		if(NAMES_TO_EXCLUDE.contains(emp.getFirstName()) || NAMES_TO_EXCLUDE.contains(emp.getLastName())) {
+			return null;
+		}
+		
+		return emp;
+	}
+
+}
