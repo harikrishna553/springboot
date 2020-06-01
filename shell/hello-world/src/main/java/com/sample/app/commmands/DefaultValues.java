e1e91de92bafda39e4d49aa1bb285bb978fa749a
Harikrishna <h0g01ex@homeoffice.wal-mart.com>
2020-06-01 13:45:43 +0530

    Optional Parameters and Default Values

---
+++ shell/hello-world/src/main/java/com/sample/app/commmands/DefaultValues.java
@@ -0,0 +1,19 @@
+package com.sample.app.commmands;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent(value = "Specify Default Values using using @ShellOption")
+public class DefaultValues {
+
+	@ShellMethod(value = "Commands to greet user", key = "greet-user", prefix = "-")
+	public String greetUser(@ShellOption(defaultValue = "User") String name) {
+
+		StringBuilder builder = new StringBuilder();
+
+		builder.append("Hello ").append(name).append("!!!!");
+
+		return builder.toString();
+	}
+}
