2493256eccbdf4734009e4e5811e22e875d86372
Harikrishna <h0g01ex@homeoffice.wal-mart.com>
2020-06-01 14:30:53 +0530

    Multi Valued Parameters Demo

---
+++ shell/hello-world/src/main/java/com/sample/app/commmands/MultiValuedParametersDemo.java
@@ -0,0 +1,23 @@
+package com.sample.app.commmands;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent(value = "Multi Valued Parameters Demo")
+public class MultiValuedParametersDemo {
+
+	@ShellMethod(value = "Commands to Calculate Square of given numbers", key = "square", prefix = "-")
+	public String SquarelOfNumbers(@ShellOption(arity=5)int[] input, String message) {
+
+		StringBuilder builder = new StringBuilder();
+
+		for (int i : input) {
+			int result = i * i;
+				
+			builder.append(String.format(message, i, result)).append("\n");
+		}
+
+		return builder.toString();
+	}
+}
