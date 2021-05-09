306a4c5aafafc99fc945ee3260ec39ed898fa7a2
Harikrishna <h0g01ex@homeoffice.wal-mart.com>
2021-05-09 22:09:13 +0530

    Save multiple entities

---
+++ arangodb/save-all-demo/src/main/java/com/sample/app/App.java
@@ -0,0 +1,35 @@
+package com.sample.app;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.sample.app.entity.Employee;
+import com.sample.app.repository.EmployeeRepository;
+
+@SpringBootApplication
+public class App {
+	public static void main(String args[]) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(EmployeeRepository employeeRepo) {
+		return (args) -> {
+
+			List<Employee> emps = Arrays.asList(new Employee(1, "Ram", "Gurram"), new Employee(2, "Gopi", "Battu"),
+					new Employee(3, "Sailu", "Ptr"));
+
+			Iterable<Employee> savedEmps = employeeRepo.saveAll(emps);
+
+			for (Employee emp : savedEmps) {
+				System.out.println(emp);
+			}
+
+		};
+	}
+}
