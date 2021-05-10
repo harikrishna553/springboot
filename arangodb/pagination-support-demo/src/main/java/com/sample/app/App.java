0d298e078149227816feacaeb81e3eff21cebc4f
Harikrishna <h0g01ex@homeoffice.wal-mart.com>
2021-05-10 11:01:49 +0530

    Pagination support demo

---
+++ arangodb/pagination-support-demo/src/main/java/com/sample/app/App.java
@@ -0,0 +1,40 @@
+package com.sample.app;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import com.sample.app.entity.Employee;
+import com.sample.app.repository.EmployeeRepository;
+
+@SpringBootApplication
+public class App {
+	public static void main(String args[]) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(EmployeeRepository employeeRepo) {
+		return (args) -> {
+			List<Employee> emps = Arrays.asList(new Employee(1, "Ram", "Gurram", 31),
+					new Employee(2, "Sundar", "Deavanagari", 32), new Employee(3, "Srinuvasa rao", "Guma", 43),
+					new Employee(4, "Madhavi Latha", "Gumma", 41), new Employee(5, "Gopi", "Battu", 33),
+					new Employee(6, "Sailu", "PTR", 33), new Employee(7, "Venkat", "PTR", 32));
+
+			employeeRepo.saveAll(emps);
+
+			Iterable<Employee> empIterable = employeeRepo
+					.findAll(PageRequest.of(0, 5, Sort.by(Sort.Direction.ASC, "age")));
+
+			for (Employee emp : empIterable) {
+				System.out.println(emp);
+			}
+		};
+	}
+}
