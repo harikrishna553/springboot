f7233f3b5e49c2fa473147ce8d5b4316c6e163d4
Harikrishna <h0g01ex@homeoffice.wal-mart.com>
2021-05-10 11:41:29 +0530

    Get document by example

---
+++ arangodb/documents-by-example-demo/src/main/java/com/sample/app/config/ArangoConfig.java
@@ -0,0 +1,21 @@
+package com.sample.app.config;
+
+import com.arangodb.ArangoDB;
+import com.arangodb.springframework.annotation.EnableArangoRepositories;
+import com.arangodb.springframework.config.ArangoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableArangoRepositories(basePackages = { "com.sample.app" })
+public class ArangoConfig implements ArangoConfiguration {
+
+	@Override
+	public ArangoDB.Builder arango() {
+		return new ArangoDB.Builder().host("localhost", 8529).user("root").password("tiger");
+	}
+
+	@Override
+	public String database() {
+		return "abc_org";
+	}
+}
