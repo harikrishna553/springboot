8e649130e3cbc6735bd91f84b4a7dc7398deb049
Harikrishna <h0g01ex@homeoffice.wal-mart.com>
2021-05-16 11:05:46 +0530

    Count number of documents in the collection

---
+++ arangodb/collection-operations-count-documents/src/main/java/com/sample/app/App.java
@@ -0,0 +1,53 @@
+package com.sample.app;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.arangodb.springframework.core.ArangoOperations;
+import com.arangodb.springframework.core.CollectionOperations;
+import com.sample.app.entity.Employee;
+
+@SpringBootApplication
+public class App {
+
+	@Autowired
+	private ArangoOperations arangoTemplate;
+
+	public static void main(String args[]) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo() {
+
+		return (args) -> {
+			Employee emp1 = new Employee(1, "Krishna", "Gurram", 33);
+			Employee emp2 = new Employee(2, "Lahari", "G", 20);
+			Employee emp3 = new Employee(3, "Thulasi", "G", 19);
+
+			arangoTemplate.insert(Arrays.asList(emp1, emp2, emp3), Employee.class);
+
+			Iterable<Employee> empsIterable = arangoTemplate.findAll(Employee.class);
+
+			System.out.println("Documents in employees collection");
+			for (Employee emp : empsIterable) {
+				System.out.println(emp);
+			}
+
+			CollectionOperations empsCollection = arangoTemplate.collection(Employee.class);
+			
+			long noOfDocuments = empsCollection.count();
+			System.out.println("Total number of documents in the collection are : " + noOfDocuments);
+			
+			System.out.println("\nDropping the collection");
+
+			empsCollection.drop();
+
+		};
+	}
+}
