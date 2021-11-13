7464f782c248b6fc9a65835ae8e8adad20265426
Harikrishna <h0g01ex@homeoffice.wal-mart.com>
2021-11-14 00:29:39 +0530

    spring-thymelead hello world application using spring tool suite

---
+++ thymeleaf/thymeleaf-helloworld/src/main/java/com/sample/app/ThymeleafHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.sample.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafHelloworldApplication.class, args);
+	}
+
+}
---
+++ thymeleaf/thymeleaf-helloworld/src/test/java/com/sample/app/ThymeleafHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
