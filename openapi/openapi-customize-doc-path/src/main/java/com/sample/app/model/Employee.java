c9271f7ce9669a21914cacc8efe0966c551e7d1f
harikrishna553 <harikrishna553@gmail.com>
2022-03-11 16:01:59 +0530

    Customize doc path

---
+++ openapi/openapi-customize-doc-path/src/main/java/com/sample/app/model/Employee.java
@@ -0,0 +1,49 @@
+package com.sample.app.model;
+
+public class Employee {
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	public Employee() {}
+
+	public Employee(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Employee [id=").append(id).append(", firstName=").append(firstName).append(", lastName=")
+				.append(lastName).append("]");
+		return builder.toString();
+	}
+
+}
