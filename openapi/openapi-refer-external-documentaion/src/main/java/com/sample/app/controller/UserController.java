3ce0ee2382b922e8816a6bbbb80a299ec0954c72
harikrishna553 <harikrishna553@gmail.com>
2022-03-17 13:49:15 +0530

    Refer external documentaiton

---
+++ openapi/openapi-refer-external-documentaion/src/main/java/com/sample/app/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.sample.app.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+
+@RestController
+@RequestMapping(value = "/api/v1/users")
+@CrossOrigin("*")
+public class UserController {
+
+	@Operation(summary = "Get the employee details by name", externalDocs = @ExternalDocumentation(url = "https://self-learning-java-tutorial.blogspot.com/2019/08/spring-boot-rest-requestparam-access.html"))
+	@GetMapping("/by-name")
+	public ResponseEntity<Map<String, Object>> infoByName(
+			@Parameter(name = "firstName", in = ParameterIn.QUERY, description = "firstName ex: krishna", required = true) @RequestParam(name = "firstName", required = false) String firstName,
+			@Parameter(name = "lastName", in = ParameterIn.QUERY, description = "lastName ex: krishna", required = true) @RequestParam(name = "lastName", required = false) String lastName) {
+
+		Map<String, Object> myDetails = new HashMap<>();
+
+		if (firstName != null) {
+			myDetails.put("firstName", firstName);
+		}
+
+		if (lastName != null) {
+			myDetails.put("lastName", lastName);
+		}
+
+		return ResponseEntity.ok(myDetails);
+
+	}
+
+}
+
