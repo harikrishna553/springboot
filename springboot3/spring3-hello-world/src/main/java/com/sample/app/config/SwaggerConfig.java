8865918a259ee8c5a3f97eded62b890744e7524d
harikrishna553 <harikrishna553@gmail.com>
2024-04-05 16:06:39 +0530

    Spring boot3 hello world application

---
+++ springboot3/spring3-hello-world/src/main/java/com/sample/app/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.sample.app.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Hello service Application", version = "v1"))
+public class SwaggerConfig {
+
+}
