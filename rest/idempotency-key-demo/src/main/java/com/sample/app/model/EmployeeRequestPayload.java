1aecb2af90f770b908d83d2d40c323de233c9bb3
harikrishna553 <harikrishna553@gmail.com>
2024-04-29 14:56:34 +0530

    Demo to use Idempotency-Key HTTP Header Field

---
+++ rest/idempotency-key-demo/src/main/java/com/sample/app/model/EmployeeRequestPayload.java
@@ -0,0 +1,23 @@
+package com.sample.app.model;
+
+public class EmployeeRequestPayload {
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
---
+++ rest/idempotency-key-demo/src/main/java/com/sample/app/model/EmployeesResponse.java
@@ -0,0 +1,17 @@
+package com.sample.app.model;
+
+import java.util.List;
+
+public class EmployeesResponse {
+
+	private List<Employee> emps;
+
+	public List<Employee> getEmps() {
+		return emps;
+	}
+
+	public void setEmps(List<Employee> emps) {
+		this.emps = emps;
+	}
+
+}
